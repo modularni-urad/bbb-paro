69b4e9dc4f1a73f8125f286b3842e3defdbb3b1d
vencax <vencax@centrum.cz>
2023-01-30 14:01:00 +0100

    fix

--- pico/pages/index.js
+++ pico/pages/index.js
@@ -5,36 +5,36 @@ export default Object.assign(C, {
   components: { ProjectCard },
   template: `
 <div v-if="loaded">
-  <div v-if="curr">
-    <div class="grid">
-      <div>        
-        <h4 class="title">{{ curr.name }}</h4>
-        <p>
-          Začátek podávání návrhů: {{ curr.submission_start | date }}<br/>
-          Konec navrhování: {{ curr.submission_end | date }}<br/>
-          Začátek ověřování návrhů: {{ curr.thinking_start | date }}<br/>
-          Začátek hlasování v anketě: {{ curr.voting_start | date }}<br/>
-          Konec hlasování: {{ curr.voting_end | date }}<br/>
-          Limit rozpočtu návrhu: {{ curr.budgetlimit }} Kč<br/>
-          Počet palečků základní podpory: {{ curr.minimum_support }}
-        </p>
-        <router-link :to="{name:'paroform'}" role="button">
-          podat/upravit projekt
-        </router-link>
-      </div>
-      <div>
-        <h4>archiv</h4>
-        <ul class="menu-list" v-if="loaded">
-          <li v-for="i,idx in archive" :key="idx">
-            <router-link :to="{name:'parocall', params: { call_id: i.id }}">
-              <h3>{{ i.name }}</h3>
-            </router-link>
-          </li>
-        </ul>
-      </div>
+  <div class="grid">
+    <div v-if="curr">        
+      <h4 class="title">{{ curr.name }}</h4>
+      <p>
+        Začátek podávání návrhů: {{ curr.submission_start | date }}<br/>
+        Konec navrhování: {{ curr.submission_end | date }}<br/>
+        Začátek ověřování návrhů: {{ curr.thinking_start | date }}<br/>
+        Začátek hlasování v anketě: {{ curr.voting_start | date }}<br/>
+        Konec hlasování: {{ curr.voting_end | date }}<br/>
+        Limit rozpočtu návrhu: {{ curr.budgetlimit }} Kč<br/>
+        Počet palečků základní podpory: {{ curr.minimum_support }}
+      </p>
+      <router-link :to="{name:'paroform'}" role="button">
+        podat/upravit projekt
+      </router-link>
     </div>
+    <div>
+      <h4>archiv</h4>
+      <ul class="menu-list" v-if="loaded">
+        <li v-for="i,idx in archive" :key="idx">
+          <router-link :to="{name:'parocall', params: { call_id: i.id }}">
+            <h3>{{ i.name }}</h3>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
     
-    <hr />
+  <hr />
+  <div v-if="curr">
     <kbd v-if="projekty.length === 0">zatím žádné návrhy nedorazily :(</kbd>
     <div v-else style="display: grid; grid-template-columns: repeat(3, 1fr); gap: 8px;">
       <ProjectCard v-for="i,idx in projekty" :key="idx" 
